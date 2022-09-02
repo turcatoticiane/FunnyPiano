0a9077fe8020a0b974203854b41169b05f0cacbe
Ticiane Turcato <turcatoticiane@gmail.com>
2022-09-02 17:12:31 +0100

    keyboard improvements

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 function playSound(elementId) {
-    document.querySelector(elementId).play();
+    
+    const element = document.querySelector(elementId);
+
+    if(element === null) {
+        return alert('Sorry, we do not have this emoji yet');
+    }
+
+    if(element.localName === 'audio') {
+        element.play();
+    }
 }
 
-const soundsList = document.querySelectorAll('button');
+const soundsList = document.querySelectorAll('.item');
 
 for (let i = 0; i < soundsList.length; i++){
     
     const item = soundsList[i];
-    const itemId = item.id;
+    const emoji = item.classList[1];
 
-    const idSound = `#${itemId}_sound`;
+    const idSound = `#${emoji}_sound`;
 
     item.onclick = function () {
         playSound(idSound);
     }
+
+    item.onkeydown = function(event) {
+
+        console.log(event);
+        
+        if(event.code === 'Enter' || event.code === 'Space') {
+            item.classList.add('active');
+        }
+    }
+
+    item.onkeyup = function() {
+        item.classList.remove('active');
+    }
+
 }
